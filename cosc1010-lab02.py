d8390a5b3dfd5b16adde2513a72baeb1997023c3
Cknacks1 <Cknacks1@uwyo.edu>
2024-09-17 22:04:51 +0000

    Truning in lab 2

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,41 +1,44 @@
-# Your Name Here
+# Colton
 # UWYO COSC 1010
-# Submission Date
+# 9/17/2024
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 10
+# People who helped me, Grady, Austin Banner
 # your
 # comments
 # here
 
-your_variable_here = "when you see this, replace it with your code"
+my_variable_here = "this is my code"
 
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print(f'hello, COSC1010')
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+cowboy="cowboy joe"
+print(cowboy.title())
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
-
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+age= "1886"
+wyo= "university of wyoming"
+print(f"The {wyo.title()} was founded in {age}")
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+x,y=5,10
+print(f"x + y = {x+y}")
+print(f"x - y = {x-y}")
+print(f"x * y = {x*y}")
+print(f"x / y = {x/y}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
@@ -46,3 +49,8 @@ print(f"x / y = {your_variable_here}")
         # last_name, which is your last name
         # space, which is a space character 
     # Use string concatenation to print out your full name 
+first_name= "Colton"
+last_name= "knackstedt"
+space= ' '
+
+print(first_name+space+last_name)
